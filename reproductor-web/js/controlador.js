3b796c37ddc9fb95b7cbad844673786f975fa42d
Javier Amaya <47435376+JavierAmaya@users.noreply.github.com>
2019-05-05 16:52:29 -0600

    Add files via upload

---
+++ reproductor-web/js/controlador.js
@@ -0,0 +1,28 @@
+$(document).ready(function() {
+  reproducir();
+});
+
+window.onload=function(){
+ 
+  // creamos el objeto audio
+  var audioElement = document.createElement('audio');
+
+  // indicamos el archivo de audio a cargar
+  audioElement.setAttribute('src', 'data/glorioso.mp3');
+
+  // Si deseamos que una vez cargado empieze a sonar...
+  //audioElement.setAttribute('autoplay', 'autoplay');
+
+  document.getElementById("play").addEventListener("click", function() {
+    // Si deseamos que inicie siempre desde el principio
+    //audioElement.currentTime = 0;
+
+    // iniciamos el audio
+    audioElement.play();
+  });
+
+  document.getElementById("pause").addEventListener("click", function() {
+    // hacemos pausa
+    audioElement.pause();
+  });
+};
